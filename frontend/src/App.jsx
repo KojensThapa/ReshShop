b53794c34041085009851ceac4d5c69df835c55a
KojensThapa <magarnaryanthapa20560820@gmail.com>
2025-08-16 12:12:57 +0545

    Add the Toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./Components/Layout/UserLayout";
 import Home from "./Pages/Home";
-
+import {Toaster} from "sonner"
 const App = () => {
   return (
     <BrowserRouter>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UserLayout />}>
           {/* user routes */}
